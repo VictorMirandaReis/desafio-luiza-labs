8499e038dd777620e7f1aa635b74c8c7e98ec43c
Victor <miranda.vic@hotmail.com>
2025-05-20 13:48:48 -0300

    fix: remove invalid spaces from script arguments entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ echo "🔧 Creating DB structure..."
 PYTHONPATH=/app python src/db/migrate.py
 
 echo "🚀 Starting Server..."
-exec uvicorn src.main:app --host 0.0.0.0 --port 8000 --reload
+exec uvicorn src.main:app --host 0.0.0.0 --port 8000 --reload
